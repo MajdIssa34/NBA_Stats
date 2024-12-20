1d969be8aec919ac21a16cd0f98d01be86e97d61
MajdIssa34 <issamajd00@gmail.com>
2024-12-20 22:11:18 +1100

    Finished the backend

--- nba_players/nba_players/src/main/java/com/nba/nba_players/player/player/player.java
+++ nba_players/nba_players/src/main/java/com/nba/nba_players/player/player/player.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="player_stats")
+@Table(name="player_statistics")
 public class player {
     @Id
     @Column(name = "player_name")
