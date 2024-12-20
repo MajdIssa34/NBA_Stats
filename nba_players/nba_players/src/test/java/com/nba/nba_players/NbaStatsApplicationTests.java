77aedde2e1bc97122d6ea2601550b570cd6f6162
MajdIssa34 <issamajd00@gmail.com>
2024-12-20 19:26:48 +1100

    Initial commit

---
+++ nba_players/nba_players/src/test/java/com/nba/nba_players/NbaStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.nba_players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
